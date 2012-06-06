68d5e33fec205047c279946e9c91ad7432668aa2
Mike Willbanks <mike@digitalstruct.com>
2012-06-06 09:55:54 -0500

    updated module.php for zf2 beta4

--- Module.php
+++ Module.php
@@ -8,33 +8,16 @@
 
 namespace mwGearman;
 
-use Zend\Module\Manager,
-    Zend\Module\Consumer\AutoloaderProvider;
-
 /**
  * Module Setup
  * 
  * @category   mwGearman
  * @package    Module
  */
-class Module implements AutoloaderProvider
+class Module
 {
-    /**
-     * Set Autoloader Configuration
-     *
-     * @return array
-     */
-    public function getAutoloaderConfig()
+    public function getConfig()
     {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
+        return include __DIR__ . '/config/module.config.php';
     }
 }
