3bd8ec3b34de6c2da97cd33f37e522cd6d29450f
Mike Willbanks <mike@digitalstruct.com>
2012-06-06 10:19:10 -0500

    updated module.php to implement the module autoloader

--- Module.php
+++ Module.php
@@ -8,16 +8,42 @@
 
 namespace mwGearman;
 
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
 /**
  * Module Setup
  * 
  * @category   mwGearman
  * @package    Module
  */
-class Module
+class Module implements AutoloaderProviderInterface
 {
+    /**
+     * Get Config
+     *
+     * @return array
+     */
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+
+    /**
+     * Set Autoloader Configuration
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
     }
 }
